9ed40815109a27742071b2824a072e7817a3e9cb
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-09 17:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this.species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species; // Use private field for encapsulation
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
+
+  makeSound() {
+    super.makeSound(); // Call parent's makeSound() for basic sound
+    this.purr(); // Add cat-specific purring
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
+
+  makeSound() {
+    super.makeSound(); // Call parent's makeSound() for basic sound
+    this.bark(); // Add dog-specific barking
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
